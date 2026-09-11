5c2ea86ddb228ecc7df8586002c8cb0753abdfc7
rafaelfra34 <rafa3451@gmail.com>
2026-09-11 14:21:15 -0300

    Add PressFran v2.3 financial dashboard and customizable branding

---
+++ v23files/FinanceSummary.java
@@ -0,0 +1,25 @@
+package com.pressfran.cobranzas;
+
+public class FinanceSummary {
+    public double capitalEnCalle;
+    public double interesPendiente;
+    public double dineroEnCalle;
+    public double montoAtrasado;
+    public double moraAcumulada;
+    public double cobradoHoy;
+    public double cobradoMes;
+    public double capitalRecuperadoMes;
+    public double interesCobradoMes;
+    public double moraCobradaMes;
+    public double otrosIngresosMes;
+    public double gastosMes;
+    public double gananciaRealMes;
+    public double gananciaProyectada;
+    public double cajaDisponible;
+    public double rendimientoMes;
+    public double morosidadPct;
+    public double ticketPromedio;
+    public int prestamosActivos;
+    public int clientesActivos;
+    public int prestamosAtrasados;
+}
